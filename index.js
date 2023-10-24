0f04d09e51df1095e9ab1e58c24c6af47dd813d6
John Heger <heger7@gmail.com>
2023-10-24 13:30:14 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-// Your code here
+// Write your solution here!
+
 const dodger = document.getElementById("dodger");
+
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-
+  
     if (left > 0) {
-        dodger.style.left = `${left - 1}px`;
+      dodger.style.left = `${left - 1}px`;
     }
-}
-document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-        moveDodgerLeft();
-    }
-});
+  }
 
-function moveDodgerRight() {
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
 
     if (left > 0) {
-        dodger.style.left = `${left + 1}px`;
+        dodger.style.left = `${left + 1}px`
     }
-}
-document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowRight") {
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if(e.key = "ArrowRight") {
         moveDodgerRight();
     }
-});
+  });
+
